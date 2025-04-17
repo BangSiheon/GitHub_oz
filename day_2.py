d6e84e49111eb24558ab0f10f36242de6ec809c3
BangSiheon <ph05217@naver.com>
2025-04-18 00:31:51 +0900

    Python 2일차 과제

---
+++ day_2.py
@@ -0,0 +1,19 @@
+sum = 1 + 2
+sum = sum + 3
+sum = sum + 4
+sum = sum + 5
+print(sum)
+
+sum2 = 1 + 2
+sum2 += 3
+sum2 += 4
+sum2 += 5
+print(sum2)
+
+box__shoes = "스니커즈"
+box__hat = "슈프림 모자"
+print(box__shoes)
+print(box__hat)
+
+me = "시헌"
+print(f'{me}만 빼고 고양이 다 있어')
